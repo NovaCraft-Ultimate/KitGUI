c13d9013886916451447de8a622275f66e12766e
Spideynn <spideynn@gmail.com>
2016-10-14 13:59:26 -0400

    Started work on the friends system.

--- src/main/java/net/spideynn/bukkit/kitgui/gui/FriendsMenu.java
+++ src/main/java/net/spideynn/bukkit/kitgui/gui/FriendsMenu.java
@@ -1,19 +1,40 @@
 package net.spideynn.bukkit.kitgui.gui;
 
+import net.spideynn.bukkit.kitgui.Main;
 import net.spideynn.bukkit.kitgui.guilib.events.ItemClickEvent;
 import net.spideynn.bukkit.kitgui.guilib.items.BackItem;
 import net.spideynn.bukkit.kitgui.guilib.items.MenuItem;
 import net.spideynn.bukkit.kitgui.guilib.menus.ItemMenu;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.UUID;
+
 public class FriendsMenu extends ItemMenu {
-    public FriendsMenu(JavaPlugin plugin, ItemMenu parent) {
-        super("Friends", Size.ONE_LINE, plugin, parent);
+    public FriendsMenu(JavaPlugin plugin, ItemMenu parent, Player opener) {
+        super("Friends", Size.TWO_LINE, plugin, parent);
         this.setItem(8, new BackItem(Material.BARRIER));
         this.setItem(0, new AddFriendItem());
+        net.spideynn.bukkit.kitgui.mongodb.Player playerDb = Main.db.getUserByPlayer(opener);
+        int counter = 1;
+        for (String uuid : playerDb.friends) {
+            Player player = Bukkit.getPlayer(UUID.fromString(uuid));
+            if (player.isOnline()) {
+                ItemStack skull = new ItemStack(Material.SKULL_ITEM, 1);
+                skull.setDurability((short) 3);
+                SkullMeta skullMeta = (SkullMeta) skull.getItemMeta(); // no arguments needed
+                skullMeta.setDisplayName(player.getDisplayName()); // this line isn't needed
+                skullMeta.setOwner(player.getName());
+                skull.setItemMeta(skullMeta);
+                this.setItem(counter, new MenuItem(Bukkit.getPlayer(UUID.fromString(uuid)).getDisplayName(), skull));
+                counter++;
+            }
+        }
     }
 }
 
@@ -26,8 +47,8 @@ class AddFriendItem extends MenuItem {
     @Override
     public void onItemClick(ItemClickEvent event) {
         event.setWillClose(true);
-        event.getPlayer().sendMessage(ChatColor.AQUA + "Enter your friends Minecraft username.");
-        event.getPlayer().sendMessage(ChatColor.AQUA + "Must be an online player.");
+        event.getPlayer().sendMessage(ChatColor.AQUA + "> Enter your friends Minecraft username.");
+        event.getPlayer().sendMessage(ChatColor.AQUA + "> It must be an online player.");
         MainGUI.addFriendPrompt.put(event.getPlayer(), true);
     }
 }
--- src/main/java/net/spideynn/bukkit/kitgui/listeners/Listeners.java
+++ src/main/java/net/spideynn/bukkit/kitgui/listeners/Listeners.java
@@ -3,6 +3,8 @@ package net.spideynn.bukkit.kitgui.listeners;
 import net.spideynn.bukkit.kitgui.Main;
 import net.spideynn.bukkit.kitgui.gui.MainGUI;
 import net.spideynn.bukkit.kitgui.mongodb.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -35,7 +37,14 @@ public class Listeners implements Listener {
     @EventHandler
     public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent event) {
         if(MainGUI.addFriendPrompt.containsKey(event.getPlayer())) {
-            // TODO: Add friend here
+            if (Main.db.getUserByPlayer(event.getPlayer()).friends.size() >= 16) {
+                event.getPlayer().sendMessage(ChatColor.RED + "You have hit the limit of 16 friends. (for now)");
+            }
+            org.bukkit.entity.Player friend = Bukkit.getPlayer(event.getMessage());
+            Player playerDb = Main.db.getUserByPlayer(event.getPlayer());
+            playerDb.addFriend(friend);
+            Main.db.savePlayer(playerDb);
+            MainGUI.addFriendPrompt.remove(event.getPlayer());
         }
     }
 }
