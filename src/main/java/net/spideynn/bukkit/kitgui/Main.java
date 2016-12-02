772e0a716b8bfe42d4c924132549e2162437749c
Spideynn <spideynn@gmail.com>
2016-12-02 08:41:07 -0500

    Rearranged some code

--- src/main/java/net/spideynn/bukkit/kitgui/Main.java
+++ src/main/java/net/spideynn/bukkit/kitgui/Main.java
@@ -47,42 +47,54 @@ public class Main extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (label.equalsIgnoreCase("kitgui")) {
-            if (sender instanceof Player) {
-                Player player = (Player) sender;
-                if (args.length == 2 && args[0].equalsIgnoreCase("unlockall")) {
-                    Player p = Bukkit.getPlayer(args[1]);
-                    if (p.getPlayer() == null) {
-                        sender.sendMessage(ChatColor.RED + "Player is not online or does not exist.");
-                        return true;
-                    }
-                    net.spideynn.bukkit.kitgui.mongodb.Player pl = db.getUserByPlayer(p.getPlayer());
-                    for (Kits kit : Kits.values()) {
-                        pl.kits.add(kit.getKitNum());
-                    }
-                    Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> db.savePlayer(pl));
-                    player.getPlayer().sendMessage("Unlocked all the kits for " + args[1] + ".");
-                    return true;
-                } else if (args.length == 2 && args[0].equalsIgnoreCase("lockall")) {
-                    Player p = Bukkit.getPlayer(args[1]).getPlayer();
-                    if (p.getPlayer() == null) {
-                        sender.sendMessage(ChatColor.RED + "Player is not online or does not exist.");
-                        return true;
-                    }
-                    net.spideynn.bukkit.kitgui.mongodb.Player pl = db.getUserByPlayer(p.getPlayer());
-                    pl.kits = new ArrayList<>();
-                    Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> db.savePlayer(pl));
-                    p.getPlayer().sendMessage("Locked all the kits for " + args[1] + ".");
-                    return true;
-                } else if (args.length == 2 && args[0].equalsIgnoreCase("debug")) {
-                    Player p = Bukkit.getPlayer(args[1]).getPlayer();
-                    if (p.getPlayer() == null) {
-                        sender.sendMessage(ChatColor.RED + "Player is not online or does not exist.");
-                        return true;
-                    }
-                    net.spideynn.bukkit.kitgui.mongodb.Player pl = db.getUserByPlayer(p.getPlayer());
-                    sender.sendMessage(pl.toString());
+            if (args.length == 2 && args[0].equalsIgnoreCase("unlockall")) {
+                if (!sender.hasPermission("kitpvp.admin")) {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission to run this command.");
                     return true;
                 }
+                Player p = Bukkit.getPlayer(args[1]);
+                if (p.getPlayer() == null) {
+                    sender.sendMessage(ChatColor.RED + "Player is not online or does not exist.");
+                    return true;
+                }
+                net.spideynn.bukkit.kitgui.mongodb.Player pl = db.getUserByPlayer(p.getPlayer());
+                for (Kits kit : Kits.values()) {
+                    pl.kits.add(kit.getKitNum());
+                }
+                Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> db.savePlayer(pl));
+                sender.sendMessage("Unlocked all the kits for " + args[1] + ".");
+                return true;
+            } else if (args.length == 2 && args[0].equalsIgnoreCase("lockall")) {
+                if (!sender.hasPermission("kitpvp.admin")) {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission to run this command.");
+                    return true;
+                }
+                Player p = Bukkit.getPlayer(args[1]).getPlayer();
+                if (p.getPlayer() == null) {
+                    sender.sendMessage(ChatColor.RED + "Player is not online or does not exist.");
+                    return true;
+                }
+                net.spideynn.bukkit.kitgui.mongodb.Player pl = db.getUserByPlayer(p.getPlayer());
+                pl.kits = new ArrayList<>();
+                Bukkit.getScheduler().runTaskAsynchronously(Main.getInstance(), () -> db.savePlayer(pl));
+                p.getPlayer().sendMessage("Locked all the kits for " + args[1] + ".");
+                return true;
+            } else if (args.length == 2 && args[0].equalsIgnoreCase("debug")) {
+                if (!sender.hasPermission("kitpvp.admin")) {
+                    sender.sendMessage(ChatColor.RED + "You do not have permission to run this command.");
+                    return true;
+                }
+                Player p = Bukkit.getPlayer(args[1]).getPlayer();
+                if (p.getPlayer() == null) {
+                    sender.sendMessage(ChatColor.RED + "Player is not online or does not exist.");
+                    return true;
+                }
+                net.spideynn.bukkit.kitgui.mongodb.Player pl = db.getUserByPlayer(p.getPlayer());
+                sender.sendMessage(pl.toString());
+                return true;
+            }
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
                 MainGUI mainGUI = new MainGUI(this, player);
                 mainGUI.open(player);
                 return true;
